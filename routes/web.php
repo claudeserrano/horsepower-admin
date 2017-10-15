6031ab814ffed32b14e2195878196c2e957ced73
claudeserrano <32248305+claudeserrano@users.noreply.github.com>
2017-10-15 10:28:47 -0400

    'update'

--- routes/web.php
+++ routes/web.php
@@ -133,19 +133,19 @@ Route::post('/regpdf', function (Request $request) {
 	$pdf->Image($sig,165,$y,-300);
 	Storage::disk('s3')->put('signature.pdf', $pdf->Output('signature.pdf', 'S'));
 
-	Storage::disk('s3')->delete('signature.png');
+	// Storage::disk('s3')->delete('signature.png');
 
-	$data = $request->all();
-	unset($data['_token']);
+	// $data = $request->all();
+	// unset($data['_token']);
 
-	$pdf = new Pdf('forms/Registration_English_Fillable.pdf');
-	$pdf->fillForm($data);
-	// $pdf->stamp(Storage::disk('s3')->url('signature.pdf'));
-	$pdf->flatten();
+	// $pdf = new Pdf('forms/Registration_English_Fillable.pdf');
+	// $pdf->fillForm($data);
+	// // $pdf->stamp(Storage::disk('s3')->url('signature.pdf'));
+	// $pdf->flatten();
 
-	Storage::disk('s3')->delete('signature.pdf');
+	// Storage::disk('s3')->delete('signature.pdf');
 
-	$pdf->execute();
+	// $pdf->execute();
 
 	// $temp = file_get_contents($pdf->getTmpFile());
 

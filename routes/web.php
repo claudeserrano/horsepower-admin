ba35e9c4a280ad186cb04b05ce178cd37d78f10f
claudeserrano <32248305+claudeserrano@users.noreply.github.com>
2017-10-16 02:37:40 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -149,7 +149,7 @@ Route::post('/regpdf', function (Request $request) {
 
 	$pdf = new Pdf('forms/Registration_English_Fillable.pdf');
 
-	return $pdf->getTmpFile();
+	return var_dump($pdf);
 	$pdf->fillForm($data);
 	// $pdf->stamp(Storage::disk('s3')->url('signaturpe.pdf'));
 	$pdf->flatten();

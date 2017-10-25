6c99ba684ded1e855733a93ec21205781b15c3a4
claudeserrano <32248305+claudeserrano@users.noreply.github.com>
2017-10-24 23:34:29 -0400

    Update

--- routes/web.php
+++ routes/web.php
@@ -33,25 +33,7 @@ Route::post('/upload', 'DashboardController@uploadFiles')->name('upload');
 
 Route::get('/ultipro/login', function(){
 
-	$LOGIN_URL = 'https://service4.ultipro.com/services/LoginService';
-	 $client = new \SoapClient($LOGIN_URL, array('soap_version' => SOAP_1_2, 'exceptions' => TRUE, 'trace' => TRUE));
-
-    $headers = array();
-    $headers[0] = new \SoapHeader('http://www.w3.org/2005/08/addressing', 'Action', 'http://www.ultipro.com/services/loginservice/ILoginService/Authenticate', true);
-    $headers[1] = new \SoapHeader('http://www.ultipro.com/services/loginservice', 'ClientAccessKey', getenv('ULTIPRO_CKEY'));
-	$headers[2] = new \SoapHeader('http://www.ultipro.com/services/loginservice', 'Password', getenv('ULTIPRO_PW'));
-	$headers[3] = new \SoapHeader('http://www.ultipro.com/services/loginservice', 'UserAccessKey', getenv('ULTIPRO_UKEY'));
-    $headers[4] = new \SoapHeader('http://www.ultipro.com/services/loginservice', 'UserName', getenv('ULTIPRO_USER'));
-
-    $client->__setSoapHeaders($headers);
-	$response = $client->Authenticate();
-
-	try {
-		return $response->Token;
-	} catch (Exception $e) {
-		return $e;	
-	}
-	// return App\Services\Ultipro::login();
+	return App\Services\Ultipro::login();
 });
 
 Route::get('/success', function(){

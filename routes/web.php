892cdd21607f5251db32a2b3f1ad7612de07eafa
claudeserrano <32248305+claudeserrano@users.noreply.github.com>
2017-10-24 23:38:44 -0400

    Update

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,45 @@ Route::post('/upload', 'DashboardController@uploadFiles')->name('upload');
 
 Route::get('/ultipro/login', function(){
 
-	return \App\Services\Ultipro::login();
+	// return \App\Services\Ultipro::login();
+	        
+			$curl = curl_init();
+
+	        curl_setopt_array($curl, array(
+	          CURLOPT_URL => "https://service4.ultipro.com/services/LoginService",
+	          CURLOPT_RETURNTRANSFER => true,
+	          CURLOPT_ENCODING => "",
+	          CURLOPT_MAXREDIRS => 10,
+	          CURLOPT_TIMEOUT => 30,
+	          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	          CURLOPT_CUSTOMREQUEST => "POST",
+	          CURLOPT_POSTFIELDS => "<s:Envelope xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\" xmlns:a=\"http://www.w3.org/2005/08/addressing\">\r\n  <s:Header>\r\n<a:Action   s:mustUnderstand=\"1\">http://www.ultipro.com/services/loginservice/ILoginService/Authenticate</a:Action> \r\n  \t    <h:ClientAccessKey xmlns:h=\"http://www.ultipro.com/services/loginservice\">4CNBB</h:ClientAccessKey> \r\n  \t    <h:Password xmlns:h=\"http://www.ultipro.com/services/loginservice\">Wireless1!</h:Password> \r\n  \t    <h:UserAccessKey xmlns:h=\"http://www.ultipro.com/services/loginservice\">BVWFOG0000K0</h:UserAccessKey> \r\n  \t    <h:UserName xmlns:h=\"http://www.ultipro.com/services/loginservice\">trialservice</h:UserName> \r\n \t  </s:Header>\r\n  <s:Body>\r\n \t    <TokenRequest xmlns=\"http://www.ultipro.com/contracts\" /> \r\n  </s:Body>\r\n</s:Envelope>",
+	          CURLOPT_HTTPHEADER => array(
+	            "cache-control: no-cache",
+	            "content-type: application/soap+xml; charset=utf-8",
+	            "postman-token: 6366dbfa-8487-3b6a-f3d3-404e77fa8eaf"
+	          ),
+	        ));
+
+	        try {
+
+	            $response = curl_exec($curl);
+	            $err = curl_error($curl);
+
+	            curl_close($curl);
+
+	            if ($err) {
+	              return "cURL Error #:" . $err;
+	            }
+
+	            $xml = simplexml_load_string($response);
+	            $token = $xml->children('s', true)->Body->children('http://www.ultipro.com/contracts')->children('http://www.ultipro.com/services/loginservice')->Token;
+	            
+	            return $token;
+
+	        } catch (Exception $e) {
+	            return $e;
+	        }
 });
 
 Route::get('/success', function(){
